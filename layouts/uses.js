3062d4355e93331fe8caff80f907c12d17eacbd1
Alexander Saavedra <75070098+alexandersaavedrakira@users.noreply.github.com>
2021-01-28 15:27:24 +0100

    Remove next-seo in uses layout

--- layouts/uses.js
+++ layouts/uses.js
@@ -1,20 +1,11 @@
-import { NextSeo } from 'next-seo'
-
 import Container from '@/components/Container'
 
 export default function UsesLayout ({ children }) {
   return (
-    <Container>
-      <NextSeo
-        title='Uses – Alexander Saavedra'
-        description='Aquí está toda la tecnología que estoy usando actualmente.'
-        canonical='https://mralexsaavedra.dev/uses'
-        openGraph={{
-          url: 'https://mralexsaavedra.dev/uses',
-          title: 'Uses – Alexander Saavedra',
-          description: 'Aquí está toda la tecnología que estoy usando actualmente.'
-        }}
-      />
+    <Container
+      title='Uses – Alexander Saavedra'
+      description='Aquí está toda la tecnología que estoy usando actualmente.'
+    >
       <article className='flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full'>
         <h1 className='font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white'>
           Uses
