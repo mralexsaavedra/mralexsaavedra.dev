ec6a7a5b591e1e267415ee15782944e9b070db5c
Alexander Saavedra <mralexsaavedra@gmail.com>
2023-12-21 15:13:37 +0100

    Fixed github api

--- pages/api/github.ts
+++ pages/api/github.ts
@@ -1,21 +1,61 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import type { GitHubPinnedRepo, GitHubPinnedRepos } from 'lib/types'
+import { gql, GraphQLClient } from 'graphql-request'
+
+const GITHUB_API_URL = 'https://api.github.com/graphql'
+
+const GET_PINNED_REPOS = gql`
+  query {
+    viewer {
+      pinnedItems(first: 6, types: REPOSITORY) {
+        nodes {
+          ... on Repository {
+            id
+            name
+            description
+            url
+            homepageUrl
+            languages(first: 3) {
+              nodes {
+                name
+                color
+              }
+            }
+            forkCount
+            stargazerCount
+          }
+        }
+      }
+    }
+  }
+`
+
+const getPinnedRepos = async (): Promise<GitHubPinnedRepo[]> => {
+  const graphQLClient = new GraphQLClient(GITHUB_API_URL, {
+    headers: {
+      authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`
+    }
+  })
+
+  try {
+    const data = await graphQLClient.request<GitHubPinnedRepos>(
+      GET_PINNED_REPOS,
+      {}
+    )
+    return data.viewer.pinnedItems.nodes
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const userReposResponse = await fetch(
-    'https://gh-pinned-repos.egoist.sh/?username=mralexsaavedra'
-  )
-
-  const repos = await userReposResponse.json()
-
-  res.setHeader(
-    'Cache-Control',
-    'public, s-maxage=1200, stale-while-revalidate=600'
-  )
-
-  return res.status(200).json({
-    repos
-  })
+  try {
+    const repos = await getPinnedRepos()
+    return res.status(200).json({ repos })
+  } catch (error) {
+    return res.status(500).send({ error: error.message })
+  }
 }
