417e3e56bc17745b1fa487a24af091cd99510091
Alexander Saavedra <mralexsaavedra@gmail.com>
2023-12-21 16:47:42 +0100

    Added eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: ['plugin:astro/recommended'],
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+      rules: {},
+    },
+  ],
+};
