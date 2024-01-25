47458c0986748e4776c536d79af333a0c0f6b9eb
Alexander Saavedra <mralexsaavedra@gmail.com>
2024-01-25 11:48:41 +0100

    [main]: Fixed opengraph image

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()]
+  site: "https://www.mralexsaavedra.dev",
+  integrations: [tailwind(), react(), mdx()],
 });
