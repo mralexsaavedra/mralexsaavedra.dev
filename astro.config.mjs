270fd1074ea6fb97c8876f98bcafd334031b49f9
Alexander Saavedra <mralexsaavedra@gmail.com>
2025-12-09 17:27:24 +0100

    chore: Move `@astrojs/tailwind` to devDependencies and update lockfile.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.mralexsaavedra.dev",
-  integrations: [tailwind(), react(), mdx()],
-});
+  site: 'https://www.mralexsaavedra.dev',
+  integrations: [tailwind(), react(), mdx()]
+})
