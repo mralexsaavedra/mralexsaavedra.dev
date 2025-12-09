ee50ecd2b9ed8e68a75716f57afd80b9d5e3555b
Alexander Saavedra <mralexsaavedra@gmail.com>
2025-12-09 17:49:39 +0100

    chore: remove 'uses' page, resume PDF, and MDX integration and dependencies.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import mdx from '@astrojs/mdx'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.mralexsaavedra.dev',
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind()]
 })
