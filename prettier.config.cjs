0e0e742edd5f2cf08b6729733fb52b149bb67199
Alexander Saavedra <mralexsaavedra@gmail.com>
2025-12-09 17:44:34 +0100

    chore: Integrate Prettier Tailwind CSS plugin to standardize class ordering across components and pages.

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -2,7 +2,10 @@
 module.exports = {
   ...require('prettier-config-standard'),
 
-  plugins: [require.resolve('prettier-plugin-astro')],
+  plugins: [
+    require.resolve('prettier-plugin-astro'),
+    require.resolve('prettier-plugin-tailwindcss')
+  ],
   overrides: [
     {
       files: '*.astro',
