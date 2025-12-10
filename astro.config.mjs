d1296a6299daac59deac1efce688526ec68eb39e
Alexander Saavedra <mralexsaavedra@gmail.com>
2025-12-10 10:03:10 +0100

    refactor: update site domain from .dev to .com and use environment variable for site URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.mralexsaavedra.dev',
+  site: 'https://mralexsaavedra.com',
   integrations: [tailwind()]
 })
