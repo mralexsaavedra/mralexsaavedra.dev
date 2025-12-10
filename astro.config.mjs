949b48269734028ce27af4607c22aae55f847333
Alexander Saavedra <mralexsaavedra@gmail.com>
2025-12-10 10:08:32 +0100

    feat: improve 404 page content and styling, add sitemap integration, and enhance focus visibility.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mralexsaavedra.com',
-  integrations: [tailwind()]
+  integrations: [tailwind(), sitemap()]
 })
